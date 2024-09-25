98bc01d317de46cc16c890030d73ea60b625d126
Kangning.li <kangning.li@nio.com>
2024-09-25 13:38:16 +0800

    13.1 SplashLayout basic functionality

---
+++ app/src/main/java/com/ixuea/courses/mymusic/component/splash/activity/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.ixuea.courses.mymusic.component.splash.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ixuea.courses.mymusic.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
